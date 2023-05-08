54aaaadf29672f042bb359df8ba2b4a027a5f90b
Валерия Михайлова <valeriya23.04@outlook.com>
2023-05-08 11:56:04 +0300

    Добавьте файлы проекта.

---
+++ TCPServer/ClientObject.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Reflection;
+
+namespace TCPServer
+{
+    internal class ClientObject
+    {
+        protected internal string Id { get; } = Guid.NewGuid().ToString();
+        protected internal StreamWriter Writer { get; }
+        protected internal StreamReader Reader { get; }
+
+        TcpClient client;
+        ServerObject server; // объект сервера
+
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+            client = tcpClient;
+            server = serverObject;
+            // получаем NetworkStream для взаимодействия с сервером
+            var stream = client.GetStream();
+            // создаем StreamReader для чтения данных
+            Reader = new StreamReader(stream);
+            // создаем StreamWriter для отправки данных
+            Writer = new StreamWriter(stream);
+        }
+
+        public async Task ProcessAsync()
+        {
+            try
+            {
+                // получаем имя пользователя
+                string? userName = await Reader.ReadLineAsync();
+                string? message = $"{userName} вошел в чат";
+                // посылаем сообщение о входе в чат всем подключенным пользователям
+                await server.BroadcastMessageAsync(message, Id);
+                Console.WriteLine(message);
+
+                // в бесконечном цикле получаем сообщения от клиента
+                while (true)
+                {
+                    try
+                    {
+                        message = await Reader.ReadLineAsync();
+                        if (message == null) continue;
+                        if (message.IndexOf("розовый") > -1)
+                        {
+                            message = "роза";
+                        }
+                        else if (message.IndexOf("фиолетовый") > -1)
+                        {
+                            message = "фиалка";
+                        }
+                        else if (message.IndexOf("сиреневый") > -1)
+                        {
+                            message = "сирень";
+                        }
+                        message = $"{userName}: {message}";
+
+                        Console.WriteLine(message);
+                        await server.BroadcastMessageAsync(message, Id);
+
+                    }
+
+                    catch
+                    {
+                        message = $"{userName} покинул чат";
+                        Console.WriteLine(message);
+                        await server.BroadcastMessageAsync(message, Id);
+                        break;
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // в случае выхода из цикла закрываем ресурсы
+                server.RemoveConnection(Id);
+            }
+        }
+
+
+
+        // закрытие подключения
+        protected internal void Close()
+        {
+            Writer.Close();
+            Reader.Close();
+            client.Close();
+
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using TCPServer;
+
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+
+        ServerObject server = new ServerObject();// создаем сервер
+        while (true)
+        {
+            await server.ListenAsync(); // запускаем сервер
+        }
+
+    }
+
+}
